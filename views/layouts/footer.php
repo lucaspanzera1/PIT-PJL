ee335485b057600823f579c709c8d40332cac993
Lucas Panzera <133154907+lucaspanzera1@users.noreply.github.com>
2024-11-24 20:49:38 -0300

    Delete views directory

--- views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="../../resources/css/footer.css?v=<?= time() ?>">
-<footer>
-        <div class="footer-content">
-            <p>&copy; 2024  Arena Rental, Inc. Todos os direitos reservados.</p>
-            <p><a href="https://www.instagram.com/arenarental.inc/">• Instagram</a> | <a href="#">• Política de Privacidade</a> | <a href="#">• Termos de Uso</a>  | <a href="#">• Informações da Empresa </a> </p>
-        </div>
-    </footer>
